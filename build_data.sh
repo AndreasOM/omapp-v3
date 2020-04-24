1bdf4032b8856a01c788c94f77d2d0519585fa7e
Andreas Neukoetter <andreas@omni-mad.com>
2020-04-24 23:05:30 +0200

    + Started adding base data, and build.

--- build_data.sh
+++ build_data.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env sh
+DATA=Data
+TEMP=Temp
 
-omt-asset build --content-directory Content --temp-directory Temp --data-directory Data --archive App/data/base.omar --paklist Data/data.paklist
-omt-packer pack --basepath Data --output App/data/base.omar --paklist Data/data.paklist
+if [ ! -d ${DATA} ]; then
+	mkdir -p ${DATA};
+fi
+if [ ! -d ${TEMP} ]; then
+	mkdir -p ${TEMP};
+fi
+
+omt-asset build --content-directory Content --temp-directory ${TEMP} --data-directory ${DATA} --archive App/data/base.omar --paklist ${DATA}/data.paklist
+
+# :HACK: since omt-asset doesn't write the paklist yet
+cd ${DATA}
+ls -1 |grep -v data.paklist >data.paklist
+cd -
+
+omt-packer pack --basepath ${DATA} --output App/data/base.omar --paklist ${DATA}/data.paklist
